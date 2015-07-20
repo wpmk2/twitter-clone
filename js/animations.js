452e6e8009df7eaea625749cfb19e6e3641ccce0
Wilson Parrish <wilsonparrishmk2@gmail.com>
2015-07-19 21:24:48 -0600

    twitter updates

--- js/animations.js
+++ js/animations.js
@@ -9,10 +9,17 @@ $(document).ready(function(){
 	tweetsub.hide();//css( "display", "none" );
 
 	var tweetActions = $('.tweet-actions');
-	
 	tweetActions.css('visibility', 'hidden');  //.css('display', 'none');
 	
 	var textarea = $('.tweet-compose');
+
+	$('.reply').hide();
+	$('.stats').hide();
+
+	$('body').on('mouseup', '.tweet-body', function(){
+		$('.reply', $(this).parent()).slideToggle('normal');
+		$('.stats', $(this).parent()).slideToggle('normal');		
+	});
 
 	$('body').on('mouseenter', '.tweet', function(){
 		$('.tweet-actions', this).css('visibility', 'visible');//css('display', 'inline-block');
@@ -22,20 +29,26 @@ $(document).ready(function(){
 		$('.tweet-actions', this).css('visibility', 'hidden');//css('display', 'none');
 	});
 
-	$('body').on('click', '.tweet-compose', function(){
-		$(this).css( "height", "5em" );
-		charcount.show();//css( "display", "inline" );
-		tweetsub.show();//css( "display", "inline" );
+	$('body').on('click', '.action-reply', function(){
+		$('.tweet-compose', $(this).parent().parent()).focus();
 	});
 
-	// var hideActions = function(){   
-	// 	$('tweet-actions', this).css('display', 'none');
-	// };
-// TRIED TO PASS THESE TO THE MOUSEENTER/LEAVE HANDLES BUT IT DIDNT LIKE THEM I GUESS
-	// var showActions = function(){
-	// 	alert("sadnr");
-	// 	$('tweet-actions', this).css('display', 'inline-block');
-	// };
+
+	$('body').on('click', '#user-tweet', function(){
+		$(this).animate( {height: "5em"}, 300 );
+		charcount.show();
+		tweetsub.show();
+		// alert("I work");
+	});
+
+
+	$('body').on('click', '.tweet-compose', function(){
+		$(this).animate( {height: "5em"}, 300 );
+	});
+
+	textarea.focus(function(){
+		$('#user-tweet').trigger("click");
+	});
 
 	textarea.keyup(function(){
 		var max = 40;
@@ -56,14 +69,16 @@ $(document).ready(function(){
 	    } else {
 	    	charcount.css('color', '#999');
 	    }
-	// }).keydown(function(){
-	// 	var len = $(this).val().length;
 		
+	}).keydown(function(event){
+		if ($(this).val().length > 39 && event.which == 13) {
+			event.preventDefault();
+		}
 	});
 
 	tweetsub.click(function(){
 		var tweetText = $('#tweet-content > textarea').val();
-		var newTweet = {
+		var newTweetObj = {
 			avatarURL: "img/alagoon.jpg",
 			fullName: "Wilson Parrish",
 			userName: "@the_coolest",
@@ -74,10 +89,11 @@ $(document).ready(function(){
 			tweetTimeStamp: "1:04 PM - 19 Sep 13",
 			replyPlaceholder: "Reply to @the_coolest"
 		};
-		makeNewTweet(newTweet);
+		makeNewTweet(newTweetObj);
 		$('.tweet-actions').css('visibility', 'hidden');
 		$('#user-tweet').val("").focus();
-		charcount.text(40).css('color', '<div id="999"></div>');
+		charcount.text(40);
+		// charcount.css('color', '#999'); //WHY WONT THIS BE GREY
 	});
 
 	var getFavoriters = function(){
@@ -85,20 +101,24 @@ $(document).ready(function(){
 		return avatars;
 	};
 
+
+
 	var makeNewTweet = function(tweet){
 		var tweet = '<div class="tweet">' +
 	'<div class="content">' +
-		'<img class="avatar" src="' + tweet.avatarURL + '">' +		
-		'<strong class="fullname">' + tweet.fullName + '</strong>' +
-		'<span class="username">&nbsp;' + tweet.userName + '</span>' +
-		'<p class="tweet-text">' + tweet.tweetText + '</p>' +
-		'<div class="tweet-actions">' +
-			'<ul>' +
-				'<li><span class="icon action-reply"></span> Reply</li>' +
-				'<li><span class="icon action-retweet"></span> Retweet</li>' +
-				'<li><span class="icon action-favorite"></span> Favorite</li>' +
-				'<li><span class="icon action-more"></span> More</li>' +
-			'</ul>' +
+		'<div class="tweet-body">' +
+			'<img class="avatar" src="' + tweet.avatarURL + '">' +		
+			'<strong class="fullname">' + tweet.fullName + '</strong>' +
+			'<span class="username">&nbsp;' + tweet.userName + '</span>' +
+			'<p class="tweet-text">' + tweet.tweetText + '</p>' +
+			'<div class="tweet-actions">' +
+				'<ul>' +
+					'<li><span class="icon action-reply"></span> Reply</li>' +
+					'<li><span class="icon action-retweet"></span> Retweet</li>' +
+					'<li><span class="icon action-favorite"></span> Favorite</li>' +
+					'<li><span class="icon action-more"></span> More</li>' +
+				'</ul>' +
+			'</div>' +
 		'</div>' +
 		'<div class="stats">' +
 			'<div class="retweets">' +
@@ -121,34 +141,38 @@ $(document).ready(function(){
 	'</div>' +
 '</div>';
 		$('#stream').prepend(tweet);
-	};
+		$('.reply').hide();
+		$('.stats').hide();
+	};	
+
+});
 
 
 
+//####################### WORKSHOP #########################
 
 
-//  ######### Antequated version #########
 
-	// $("textarea").on('keyup', function () {
-	//     var max = 140;
-	//     var len = $(this).val().length;	    
-	//     var char = max - len;
-	//     if (len >= max) {
-	//     	// val.value = val.value.substring(0, 140);
-	//     	$("#tweet-submit").prop('disabled', true);
-	//     	$("tweet-submit").css("background color", "rgba(0,0,255,0.1)");;
-	//     } else {
-	//     	$("#tweet-submit").prop('disabled', false);
- //    	}
- //    	$charcount.text(char);
- //    	if(len > 129) {
-	//     	$charcount.css('color', 'red');
-	//     } else {
-	//     	$charcount.css('color', '#999');
-	//     }
-
-	// });	
+// var makeNewTweet = $(function(tweet){
+// 	var tweetClone = $('.stream:first-child').clone();
+// 	}).prependTo("#stream";
+ 
+// 	tweetsub.click(function(){
+// 		var newTweet = $('.newTweet'); 
+// 		var newTweetObj = {
+// 			avatarURL: "img/alagoon.jpg",
+// 			fullName: "Wilson Parrish",
+// 			userName: "@the_coolest",
+// 			tweetText: tweetText,
+// 			reTweetNum: "100",
+// 			favorites: "9",
+// 			favoriters: getFavoriters(),
+// 			tweetTimeStamp: "1:04 PM - 19 Sep 13",
+// 			replyPlaceholder: "Reply to @the_coolest"
+// 		};
+// 		$('.stream:first-child').clone();
 
 	
 
-});
+// 	}).prependTo("#stream");
+
